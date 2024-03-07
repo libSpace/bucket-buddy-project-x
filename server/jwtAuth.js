0f9106b606387f5cb80a67a437f6c00968075013
libSpace <malikmphomalope@gmail.com>
2024-03-07 13:46:47 +0200

    Added a destructure body, and some variable

--- server/jwtAuth.js
+++ server/jwtAuth.js
@@ -1,3 +1,30 @@
 const router = require("express").Router()
+const pool = require("./db")
+
+
+//Registering
+router.post("/", async (req, res) => {
+    try {
+        
+        //1. destructure the req.body (username, password)
+        
+        const { username , password} = req.body
+
+
+        //2.Check if user exist (If user exist thenthrow error)
+
+        const user = await pool.query()
+        //3. Bcrypt the user password
+
+        //Enter  the new user inside the database
+
+        //5. generating our jwt token
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Bucket buddy server Error")
+    }
+})
+
 
 module.exports = router
