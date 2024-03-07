7b2bad3a5942a5e9ae1bb3cb2f5994653022cd45
libSpace <malikmphomalope@gmail.com>
2024-03-07 09:53:06 +0200

    Middleware and routes update

---
+++ server/index.js
@@ -0,0 +1,35 @@
+// Import the Express framework
+const express = require('express');
+
+// Create an Express application
+const app = express();
+
+// Import the CORS middleware
+const cors = require("cors");
+
+
+
+// Set the port number to 3000
+const port = 3000;
+
+
+//middleware
+//###########################
+// Enable JSON parsing for incoming requests
+app.use(express.json()); // req.body
+// Enable Cross-Origin Resource Sharing (CORS)
+app.use(cors());
+
+//ROUTES
+//Register and Login routes
+app.use("/auth", require("./jwtAuth"))
+
+
+
+
+
+// Define a route for the root URL
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// Start the application and listen on port 3000
+app.listen(port, () => console.log(`Bucket buddy app listening on port ${port}!`));
